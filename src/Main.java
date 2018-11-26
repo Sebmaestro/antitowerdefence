aaa1729d86edf3182761d3512971f3cc6a41809c
sebastian <sebbe@arledal.com>
2018-11-26 14:40:08 +0100

    La till en printsats

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Dennis har gjort sin commit");
         System.out.println("Ny commit");
+        System.out.println("hej");
 
     }
 }
