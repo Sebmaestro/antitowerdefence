3ee965f19888057d3ac4af6aa5b80f7e5ce56e45
sebastian <sebbe@arledal.com>
2018-11-27 14:22:06 +0100

    La till position klass

---
+++ src/model/Position.java
@@ -0,0 +1,92 @@
+package model;
+
+import java.util.Objects;
+
+public class Position {
+    private int x;
+    private int y;
+
+    /**
+     * Constructs a position
+     * @param x - position x
+     * @param y - position y
+     */
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns x
+     * @return x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns y
+     * @return y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Returns position to south of current position
+     * @return Position
+     */
+    public Position getPosToSouth() {
+        return new Position(x, y+1);
+    }
+
+    /**
+     * Returns position to north of current position
+     * @return Position
+     */
+    public Position getPosToNorth() {
+        return new Position(x, y-1);
+    }
+
+    /**
+     * Returns position to west of current position
+     * @return Position
+     */
+    public Position getPosToWest() {
+        return new Position(x-1, y);
+    }
+
+    /**
+     * Returns position to east of current position
+     * @return Position
+     */
+    public Position getPosToEast() {
+        return new Position(x+1, y);
+    }
+
+    /**
+     * Returns true if
+     * @param obj
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof Position) &&
+                this.getX() == ((Position)obj).getX() &&
+                this.getY() == ((Position)obj).getY())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns hashcode for given position
+     * @return int
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+}
+
