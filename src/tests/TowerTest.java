110a648aa13fd64452ce435f47a44b113ab3df0a
sebastian <sebbe@arledal.com>
2018-11-27 14:49:17 +0100

    La till paket för source code

---
+++ src/tests/TowerTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.junit.*;
+
+public class TowerTest {
+    public TowerTest() {
+
+    }
+
+    @Test
+    public void shouldBeAbleToCreateTower() {
+
+    }
+
+    @Test
+    public void shouldBeAbleToShootWhenInRange() {
+
+    }
+
+    @Test
+    public void shouldNotBeAbleToShootWhenOutOfRange() {
+
+    }
+}
---
+++ src/tests/TroopTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.*;
+
+public class TroopTest {
+    public TroopTest() {
+
+    }
+
+    @Test
+    public void shouldBeAbleToCreateTroop() {
+
+    }
+}
