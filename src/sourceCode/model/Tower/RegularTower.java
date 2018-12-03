6bcc4b703b7cb267f16b801e2be95acc6166ca46
DESKTOP-2LC3675\denni <dennis.karlman@gmail.com>
2018-12-03 16:54:52 +0100

    RegularTower

--- src/sourceCode/model/Tower/RegularTower.java
+++ src/sourceCode/model/Tower/RegularTower.java
@@ -1,7 +1,17 @@
 package sourceCode.model.Tower;
 
+import sourceCode.model.Position;
+
 /**
  * Created by denni on 2018-12-03.
  */
-public class RegularTower {
+public class RegularTower extends Tower{
+
+    public RegularTower (Position p){
+        super (p);
+        damage = 1;
+        range = 1;
+        attackPeriod = 1;
+    }
+
 }
