aee3356976a257639e5fc07057880f4aba5f8041
DESKTOP-2LC3675\denni <dennis.karlman@gmail.com>
2018-12-03 14:28:13 +0100

    trooptest

--- src/tests/model/TroopTest.java
+++ src/tests/model/TroopTest.java
@@ -1,6 +1,7 @@
 package tests.model;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 
 public class TroopTest {
     public TroopTest() {
@@ -9,6 +10,39 @@ public class TroopTest {
 
     @Test
     public void shouldBeAbleToCreateTroop() {
+        RegularTroop regTroop = new RegularTroop();
+        assertNotNull(regTroop);
+    }
 
+    @Test
+    public void shouldHaveFullHP(){
+        RegularTroop regTroop = new RegularTroop();
+        assertEquals(regTroop.getHP(), 100);
+    }
+
+    @Test
+    public void shouldBeAbleToTakeDamage(){
+        RegularTroop regTroop = new RegularTroop();
+        regTroop.recieveDamage(70);
+        assertEquals(regTroop.getHP(), 30);
+    }
+
+    @Test
+    public void shouldBeAbleToDie(){
+        RegularTroop regTroop = new RegularTroop();
+        regTroop.recieveDamage(100);
+        assertFalse(regTroop.isAlive());
+    }
+
+    @Test public void shouldHaveSpeed(){
+        RegularTroop regTroop = new RegularTroop();
+        assertEquals(regTroop.getSpeed(), 10);
+    }
+
+    @Test public void shouldBeAbleToChangeSpeed(){
+        RegularTroop regTroop = new RegularTroop();
+        regTroop.setSpeed(3);
+        assertEquals(regTroop.getSpeed(), 3);
     }
 }
+
