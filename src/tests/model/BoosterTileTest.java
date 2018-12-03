14b8fd0298410f40ca9c5e1352e29752a8bf4bf8
DESKTOP-2LC3675\denni <dennis.karlman@gmail.com>
2018-12-03 13:39:24 +0100

    boostertile test

--- src/tests/model/BoosterTileTest.java
+++ src/tests/model/BoosterTileTest.java
@@ -1,11 +1,18 @@
 package tests.model;
 
+import org.junit.Before;
 import org.junit.Test;
 import sourceCode.model.Tile.Tile;
-
 import static org.junit.Assert.*;
 
 public class BoosterTileTest {
+    Regular reg;
+    Boster booster;
+
+    @Before{
+        reg = new Regular();
+        booster = new Booster(1,1);
+    }
 
 
     Tile booster = new Booster(1, 1);
@@ -15,3 +22,10 @@ public class BoosterTileTest {
          assertNotNull(booster);
     }
 }
+
+    @Test
+    public void shoulReturnBOOSTER(){
+
+
+        assertEquals(booster.landOn().toString(), "BOOSTER");
+    }
