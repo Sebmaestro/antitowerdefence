c0c700aa6d01bdb6aaa489a9d1e1f3b6ce4a757d
DESKTOP-2LC3675\denni <dennis.karlman@gmail.com>
2018-12-04 13:20:33 +0100

    view

---
+++ src/sourceCode/view/Room.java
@@ -0,0 +1,17 @@
+package sourceCode.view;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by denni on 2018-12-04.
+ */
+public class Room {
+
+    public Room(){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+}
--- src/sourceCode/view/Screen.java
+++ src/sourceCode/view/Screen.java
@@ -8,17 +8,35 @@ import java.awt.*;
 public class Screen extends JPanel implements Runnable{
     public static int myWidth, myHeight;
     public static boolean isFirst = true;
+    public static int fpsFrame=0, fps = 30;
+    public static Room room;
     public Screen(){
 
     }
 
+    public void define(){
+        room = new Room();
+
+    }
     protected void paintComponent(Graphics g){
+        if(!isFirst){
+            define();
+
+            isFirst = false;
+        }
 
        g.clearRect(0,0, getWidth(), getHeight());
+
+        room.draw(g);
     }
 
     public void run(){
         while(true){
+            if(!isFirst){
+                define();
+
+                isFirst = false;
+            }
             repaint();
         }
     }
