8056e469731212010803609d0f5e7eb655d2750a
DESKTOP-2LC3675\denni <dennis.karlman@gmail.com>
2018-12-04 11:40:02 +0100

    main

--- src/sourceCode/Main.java
+++ src/sourceCode/Main.java
@@ -1,12 +1,12 @@
 package sourceCode;
+import sourceCode.view.View;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Dennis har gjort sin commit");
-        System.out.println("Ny commit");
-        System.out.println("hej");
-        System.out.println("simons commit");
+        View view = new View();
 
     }
 }
