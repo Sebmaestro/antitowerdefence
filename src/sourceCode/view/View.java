84df03afd83aa80211ec60dfe40f078f4f0ea534
DESKTOP-2LC3675\denni <dennis.karlman@gmail.com>
2018-12-04 11:40:26 +0100

    View

--- src/sourceCode/view/View.java
+++ src/sourceCode/view/View.java
@@ -1,5 +1,24 @@
 package sourceCode.view;
 
-public class View {
-    //placeholder
+import javax.swing.*;
+import java.awt.*;
+
+public class View extends JFrame{
+    public static String title = "Level 1";
+    public static Dimension size = new Dimension(650, 550);
+
+    public View(){
+        setTitle(title);
+        setSize(size);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        init();
+
+    }
+
+    public void init() {
+        setVisible(true);
+    }
 }
