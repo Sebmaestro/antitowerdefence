940a560492cc1e823c9a03d48fb1fee3c64e4f97
David <id15den@cs.umu.se>
2018-12-06 13:52:25 +0100

    xml-schema

---
+++ src/tests/SchemaTest.java
@@ -0,0 +1,61 @@
+package tests;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class SchemaTest {
+
+
+    public SchemaTest() throws SAXException, IOException {
+
+        String schemaLang = "http://www.w3.org/2001/XMLSchema";
+        SchemaFactory factory = SchemaFactory.newInstance(schemaLang);
+        Schema schema = factory.newSchema(new StreamSource("/Users/David/" +
+                "IdeaProjects/Javagrupp7/antitowerdefence/src/resources/schema.xsd"));
+        Validator validator = schema.newValidator();
+        File testFile = new File("/Users/David/IdeaProjects/Javagrupp7/" +
+                "antitowerdefence/src/resources/testLevel.xml");
+        CustomHandler errorHandler = new CustomHandler();
+        validator.setErrorHandler(errorHandler);
+        validator.validate(new StreamSource(testFile));
+
+        if (errorHandler.isValid()){
+            System.out.println("Xmlfile is valid");
+        } else {
+            System.out.println("Xmlfile is not valid");
+        }
+
+
+    }
+
+    class CustomHandler extends DefaultHandler {
+
+        private boolean isValid = true;
+
+        public boolean isValid() {
+            return isValid;
+        }
+        @Override
+        public void warning(SAXParseException exception) throws SAXException {
+         this.isValid = false;
+        }
+
+        @Override
+        public void error(SAXParseException exception) throws SAXException {
+            this.isValid = false;
+
+        }
+
+        @Override
+        public void fatalError(SAXParseException exception) throws SAXException {
+            this.isValid = false;
+
+        }
+    }
+}
