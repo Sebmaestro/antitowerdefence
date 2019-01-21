b0117115b3cd8738d70d764d6bf313a7cd9e977e
slundkvist93 <slundkvist93@gmail.com>
2019-01-21 15:16:25 +0100

    kommentarer

--- src/sourceCode/view/GameMenu.java
+++ src/sourceCode/view/GameMenu.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 /**
  * Created by David Eriksson / Simon Lundkvist
+ * 2019-01-21
+ * Class to represent the drop down menu bar in the top left corner.
  */
 public class GameMenu extends JMenuBar{
 
@@ -14,11 +16,17 @@ public class GameMenu extends JMenuBar{
     private JMenuItem help;
     private JMenuItem about;
 
-
+    /**
+     * Constructor: calls the createMenuBar method that initializes the menu.
+     */
     GameMenu(){
         createMenuBar();
     }
 
+    /**
+     * createMenuBar: initializes the menu bar and its fields.
+     * @return the JMenuBar
+     */
     JMenuBar createMenuBar() {
 
         JMenuBar menu2 = new JMenuBar();
@@ -46,30 +54,58 @@ public class GameMenu extends JMenuBar{
         return menu2;
     }
 
+    /**
+     * setRestartListener: sets the restart menu item listener
+     * @param actionListener an ActionListener to set the restart item to
+     */
     public void setRestartListener(ActionListener actionListener){
         restart.addActionListener(actionListener);
     }
 
+    /**
+     * setPauseListener: sets the pause menu item listener
+     * @param actionListener an ActionListener to set the pause item to
+     */
     public void setPauseListener(ActionListener actionListener){
         pause.addActionListener(actionListener);
     }
 
+    /**
+     * setQuitListener: sets the quit menu item listener
+     * @param actionListener an ActionListener to set the quit item to
+     */
     public void setQuitListener(ActionListener actionListener){
         quit.addActionListener(actionListener);
     }
 
+    /**
+     * setHelpListener: sets the help menu item listener
+     * @param actionListener an ActionListener to set the help item to
+     */
     public void setHelpListener(ActionListener actionListener){
         help.addActionListener(actionListener);
     }
 
+    /**
+     * setAboutListener: sets the about menu item listener
+     * @param actionListener an ActionListener to set the about item to
+     */
     public void setAboutListener(ActionListener actionListener){
         about.addActionListener(actionListener);
     }
 
+    /**
+     * setRestartNewGameText: sets the restart menu items text
+     * @param str a string to set the restart item text to
+     */
     public void setRestartNewGameText(String str) {
         restart.setText(str);
     }
 
+    /**
+     * setPauseText: sets the pause menu items text
+     * @param str a string to set the pause item text to
+     */
     public void setPauseText(String str) {
         pause.setText(str);
     }
