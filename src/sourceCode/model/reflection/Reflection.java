0be56494aa3d7f1bf29f94183ded071b74d52f2f
sebastian <sebbe@arledal.com>
2019-01-21 15:01:51 +0100

    Reflection kommentarer

--- src/sourceCode/model/reflection/Reflection.java
+++ src/sourceCode/model/reflection/Reflection.java
@@ -6,12 +6,20 @@ import java.lang.reflect.InvocationTargetException;
 
 /**
  * Author: Sebastian Arledal c17sal / Simon Lundkvist c17slt
+ * 2019-01-21
+ *
+ * Class to handle the reflection
  */
 public class Reflection {
     private Class<?> klass;
     private Object newKlass;
     private Position p;
 
+    /**
+     * Constructor: Creates a new class with reflection
+     * @param className - Name of class to create
+     * @param p - position
+     */
     public Reflection(String className, Position p) {
         try {
             klass = Class.forName(className);
@@ -21,6 +29,9 @@ public class Reflection {
         this.p = p;
     }
 
+    /**
+     * Creates object from the class
+     */
     private void createObject() {
         try {
             Constructor<?> con = klass.getConstructor(Position.class);
@@ -30,6 +41,10 @@ public class Reflection {
         }
     }
 
+    /**
+     * Gets the current tile as an object
+     * @return newKlass - the object
+     */
     public Object getTile() {
         createObject();
         return newKlass;
