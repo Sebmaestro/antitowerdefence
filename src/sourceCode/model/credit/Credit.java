76fb06faecd3285768f95ff6b47242c0253e65e4
sebastian <sebbe@arledal.com>
2019-01-21 14:55:08 +0100

    Credits

--- src/sourceCode/model/credit/Credit.java
+++ src/sourceCode/model/credit/Credit.java
@@ -6,15 +6,25 @@ import sourceCode.model.troop.Troop;
 
 /**
  * Author: Sebastian Arledal c17sal
+ * 2019-01-21
+ *
+ * Class to hold player credits
  */
 public class Credit {
 
     private int credits;
 
+    /**
+     * Constructor: Sets starting credit
+     */
     public Credit() {
         credits = 500;
     }
 
+    /**
+     * Buys a troop and deducts credits
+     * @param t - troop
+     */
     public void buyNewTroop(Troop t) {
         if (t instanceof RegularTroop) {
             credits = credits - 100;
@@ -25,14 +35,25 @@ public class Credit {
         }
     }
 
+    /**
+     * Receive credit from entering goal
+     */
     public void getGoalCredits() {
         credits = credits + 200;
     }
 
+    /**
+     * Gets current credits
+     * @return credits - your credits
+     */
     public int getCredits() {
         return credits;
     }
 
+    /**
+     * Sets credit for player
+     * @param newCredits - new credits
+     */
     public void setCredits(int newCredits) {
         credits = newCredits;
     }
