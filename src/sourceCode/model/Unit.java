b678dc4613505ac037612a9f245a00a07e343645
sebastian <sebbe@arledal.com>
2019-01-21 16:40:44 +0100

    Unit kommentarer

--- src/sourceCode/model/Unit.java
+++ src/sourceCode/model/Unit.java
@@ -1,6 +1,16 @@
 package sourceCode.model;
 
+/**
+ * Author: Sebastian Arledal
+ * 2019-01-21
+ *
+ * Interface Unit. Tower and troop implement this interface
+ */
 public interface Unit {
 
+    /**
+     * Gets position
+     * @return position
+     */
     Position getPosition();
 }
