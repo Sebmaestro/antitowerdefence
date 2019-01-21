173b6dbcc9f602e1c1894c33d37b093a763e5436
sebastian <sebbe@arledal.com>
2019-01-21 16:25:59 +0100

    Alla tiles kommenterade

--- src/sourceCode/model/tile/Tile.java
+++ src/sourceCode/model/tile/Tile.java
@@ -6,6 +6,9 @@ import sourceCode.model.Unit;
 
 /**
  * Author: Sebastian Arledal / Simon Lundkvist / Dennis Karlman
+ * 2019-01-21
+ *
+ * Abstract class tile that troops walk on. All tile types extends this class
  */
 public abstract class Tile implements Unit, LandOn {
     Direction dir;
@@ -14,30 +17,58 @@ public abstract class Tile implements Unit, LandOn {
     Position exitPosition;
     Direction directionAtExit;
 
+    /**
+     * Constructor: Creates new tile
+     * @param p - position
+     */
     public Tile(Position p) {
         this.p = p;
         exitPosition = p;
     }
 
+    /**
+     * Boolean to set if troop can walk or not on tile
+     * @return boolean - true or false
+     */
     public abstract boolean canWalk();
 
+    /**
+     * Gets position
+     * @return p - position
+     */
     public Position getPosition() {
         return p;
     }
 
+    /**
+     * Gets graphic of tile
+     * @return graphic - the graphic string
+     */
     public String getGraphic() {
         return graphic;
     }
 
+    /**
+     * Gets direction when exited from teleport
+     * @return directionAtExit - direction
+     */
     Direction getDirectionAtExit(){
 
         return directionAtExit;
     }
 
+    /**
+     * Sets direction when troop exits from teleport
+     * @param dir - direction
+     */
     public void setDirectionAtExit(Direction dir){
         directionAtExit = dir;
     }
 
+    /**
+     * Sets position when troop exits from teleport
+     * @param p - position
+     */
     public void setExitTPosition(Position p) {
         exitPosition = p;
     }
--- src/sourceCode/model/tile/Towerzone.java
+++ src/sourceCode/model/tile/Towerzone.java
@@ -5,19 +5,34 @@ import sourceCode.model.troop.Troop;
 
 /**
  * Author: Sebastian Arledal c17sal
+ * 2019-01-21
+ *
+ * Towerzone where towers can stand
  */
 public class Towerzone extends Tile {
 
+    /**
+     * Constructor: Creates new towerzone tile
+     * @param p - position
+     */
     public Towerzone(Position p) {
         super(p);
         graphic = "src/resources/towerzone.png";
     }
 
+    /**
+     * Boolean to set if troop can walk or not on tile
+     * @return boolean - true or false
+     */
     @Override
     public boolean canWalk() {
         return false;
     }
 
+    /**
+     * Empty landon because troops doesnt walk here
+     * @param t - troop
+     */
     @Override
     public void landOn(Troop t) {}
 }
