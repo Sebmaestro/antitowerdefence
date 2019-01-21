c0c997326f82e6d72a129201000673ab06703af5
slundkvist93 <slundkvist93@gmail.com>
2019-01-21 14:47:44 +0100

    Merge branch 'mammibranch3'

--- src/sourceCode/view/ButtonPanel.java
+++ src/sourceCode/view/ButtonPanel.java
@@ -6,6 +6,10 @@ import java.awt.event.ActionListener;
 
 /**
  * Author: Simon Lundkvist / Sebastian Arledal
+ * 2019-01-21
+ * Class to represent the button panel during the game session,
+ * containing the buttons for sending troops and displaying
+ * various information such as credits and time.
  */
 public class ButtonPanel extends JPanel {
     private JButton troop1, troop2, troop3, teleportButton;
@@ -13,9 +17,16 @@ public class ButtonPanel extends JPanel {
     private JTextField goalCounter;
     private JTextField timer;
 
+    /**
+     * Constructor: calls the define method to initialize the panel and its fields.
+     */
     ButtonPanel() {
         define();
     }
+
+    /**
+     * define: Initializes the panel and its fields.
+     */
     private void define() {
 
         troop1 = new JButton("Regular - $100");
@@ -44,6 +55,11 @@ public class ButtonPanel extends JPanel {
         add(timer);
     }
 
+    /**
+     * addActionListener: adds an ActionListener to a troop button.
+     * @param e An ActionListener that will be used
+     * @param troop The troop button which the listener will be set to.
+     */
     public void addActionListener(ActionListener e, String troop) {
         if (troop.equals("Regular")) {
             troop1.addActionListener(e);
@@ -54,18 +70,34 @@ public class ButtonPanel extends JPanel {
         }
     }
 
+    /**
+     * addSetTeleportListener: adds an ActionListener to the Set Teleport button
+     * @param e An ActionListener
+     */
     public void addSetTeleportListener(ActionListener e) {
         teleportButton.addActionListener(e);
     }
 
+    /**
+     * setMoneyField: sets the money field to a new value
+     * @param money The value to set the field to
+     */
     public void setMoneyField(int money) {
         moneyField.setText("Credits: $"+money);
     }
 
+    /**
+     * setGoalCounter: sets the goal counter to a new value
+     * @param goalCounter the value to set the goal counter to
+     */
     public void setGoalCounter(int goalCounter) {
         this.goalCounter.setText(goalCounter+"/20 troops in goal");
     }
 
+    /**
+     * setTimer: sets the timer to a new value
+     * @param time the value to set the timer to
+     */
     public void setTimer(Long time) {
         timer.setText("Time: "+time);
     }
